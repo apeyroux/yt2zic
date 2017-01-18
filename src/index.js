92d042b0cf08c34e54bec33e973cda83987b8816
Alexandre Peyroux <alex@px.io>
2017-01-18 21:56:15 +0100

    add export format option

--- src/index.js
+++ src/index.js
@@ -3,22 +3,30 @@
 import * as ytdl from 'youtube-dl'
 import fs from 'fs'
 
-var argv = require('yargs')
-    .usage('Usage: $0 --source [string] --to [string]')
-    .demandOption(['source', 'to'])
-    .argv;
+var yargs = require('yargs')
+    .usage('Usage: $0 --source [string] --to [string] --format ["best", "aac", "vorbis", "mp3", "m4a", "opus", "wav"]')
+    .demandOption(['s', 't'])
+    .default('f', 'best')
+    .alias('s', 'source')
+    .alias('t', 'to')
+    .alias('f', 'format')
+    .describe('s', 'Source file (array of url)')
+    .describe('t', 'Destination path')
+    .describe('f', 'Export format (mp3, m4a ...)')
+    .help('h'),
+    argv = yargs.argv;
 
 fs.readFile(argv.source, "utf8", function(error, urls) {
     JSON.parse(urls).map((url) => {
-	ytdl.exec(url, ['--embed-thumbnail'
-			, '-x'
-			, '--audio-format'
-			, 'mp3'], { cwd: argv.to }, function(err, output) {
-			    if (err) {
-				console.log(err)
-				process.exit(1)
-			    }
-			    console.log(output.join('\n'));
-			});
+	ytdl.exec(url, [ '-x'
+			 , '--audio-format'
+			 , argv.format
+			 , argv.format !== ('mp3'||'best') ? '--get-thumbnail' : '--embed-thumbnail'], { cwd: argv.to }, function(err, output) {
+			     if (err) {
+				 console.log(err)
+				 process.exit(1)
+			     }
+			     console.log(output.join('\n'));
+			 });
     })
 });
